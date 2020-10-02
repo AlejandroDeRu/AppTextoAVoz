a2c832537f243166b07bbd3497db89c7590ffc36
alejandroderu <alejandro.delgadoru@gmail.com>
2020-10-02 18:19:33 -0500

    Update 2/10/2020

---
+++ app/src/main/java/org/unitec/apptextoavoz/MainActivity.kt
@@ -0,0 +1,11 @@
+package org.unitec.apptextoavoz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
